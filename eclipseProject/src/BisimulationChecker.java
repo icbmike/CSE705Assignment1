5030804015702075ff36d949beea7ad31fe6c4e7
Michael Little <bio595@gmail.com>
2013-08-30 10:51:44 +1200

    Added BisimulationChecker with required methods

---
+++ eclipseProject/src/BisimulationChecker.java
@@ -0,0 +1,29 @@
+
+public class BisimulationChecker {
+
+	
+	public BisimulationChecker(){
+		
+	}
+	
+	public void readInput(String fileP, String fileQ){
+		
+	}
+	
+	public void performBisimulation(){
+		
+	}
+	
+	public void writeOutput(String filename){
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
