a363225099c14be0ece28d25887769124c1883f2
Michael Little <bio595@gmail.com>
2013-08-30 11:17:48 +1200

    started reading input files

--- eclipseProject/src/BisimulationChecker.java
+++ eclipseProject/src/BisimulationChecker.java
@@ -1,15 +1,42 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.List;
+
 
 public class BisimulationChecker {
 
 	
+	private LabeledTransitionSystem p;
+	private LabeledTransitionSystem q;
+
 	public BisimulationChecker(){
 		
 	}
 	
 	public void readInput(String fileP, String fileQ){
+		try {
+			p = createLTS(Files.readAllLines(FileSystems.getDefault().getPath(fileP), StandardCharsets.UTF_8));
+			q = createLTS(Files.readAllLines(FileSystems.getDefault().getPath(fileQ), StandardCharsets.UTF_8));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
+	private LabeledTransitionSystem createLTS(List<String> lines) {
+		LabeledTransitionSystem lts = new LabeledTransitionSystem();
+		for(String line : lines){
+			if(line.startsWith("!")) break;
+			String[] split = line.split("[,:]");
+			System.out.println(split);
+			lts.addState(1);
+			lts.addSuccesor(1, "2", 2);
+		}
+		return lts;
+	}
+
 	public void performBisimulation(){
 		
 	}
@@ -22,7 +49,6 @@ public class BisimulationChecker {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
---
+++ eclipseProject/src/LabeledTransitionSystem.java
@@ -0,0 +1,12 @@
+
+public class LabeledTransitionSystem {
+
+	public void addSuccesor(int state, String action, int successor) {
+		
+	}
+
+	public void addState(int state) {
+		
+	}
+
+}
